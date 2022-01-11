9a8f6d2e92001b282985b66de827734d086b232b
amelieflo <96128526+amelieflo@users.noreply.github.com>
2022-01-11 15:30:20 +0100

    Add files via upload

---
+++ Adresse 2 Personen.py
@@ -0,0 +1,14 @@
+#Bei diesem Code, war die Aufgabe, zwei verschiedene Adressen zu erstellen, die dann richtig untereinander auftauchen.
+
+#Mit dieser Formel, erkennt das Programm, was sie wo ersetzen müssen.
+def adresse(vorname,nachname,strasse,strassennummer,plz,ort):  
+  
+#Vorlage, dass mein Adress Beipiel geornet untereinander auftauchen wird.
+ print(vorname,nachname)       
+ print(strasse,strassennummer)
+ print(plz,ort)
+ print()
+
+#Meine Adress Beispiele, die dann schön untereinander erscheinen werden.
+adresse("Ella","Collins","avenue","57","1709","LA")    
+adresse("Younès","Dubois","soleil","4","31000","Toulouse")
